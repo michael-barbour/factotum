66105d27d9c2f8c600cf7662c88e19080ba67701
dmlyons2 <dmlyons2@gmail.com>
2017-10-05 15:57:51 -0400

    Completed AppSkel

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='/login')
 def index(request):
 	return render(request, 'dashboard/index.html')
 

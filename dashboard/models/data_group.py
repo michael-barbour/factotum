f84be012dac1bb3ea534fd6b322f1acf55309bb0
Matt Frost <mwfrost@fastmail.fm>
2017-11-21 10:36:02 -0800

    Removed default date population from downloaded_at

--- dashboard/models/data_group.py
+++ dashboard/models/data_group.py
@@ -8,7 +8,7 @@ class DataGroup(models.Model):
 	name = models.CharField(max_length=50)
 	description = models.TextField(null=True, blank=True)
 	downloaded_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-	downloaded_at = models.DateTimeField(default=timezone.now)
+	downloaded_at = models.DateTimeField()
 	extraction_script = models.CharField(max_length=250, null=True, blank=True)
 	data_source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
 	updated_at = models.DateTimeField(default=timezone.now, null=True, blank=True)

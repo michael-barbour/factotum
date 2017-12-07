739ff250df7b07553cd48af7c186ca92f84823f8
rickD <debboutr@gmail.com>
2017-12-06 16:24:29 -0800

    add validation check to csv, fix DataGroup delete form view

--- dashboard/models/data_group.py
+++ dashboard/models/data_group.py
@@ -5,6 +5,13 @@ from django.utils import timezone
 from django.core.urlresolvers import reverse
 from django.dispatch import receiver
 
+# # could be used for dynamically creating filename on instantiation
+# # in the 'upload_to' param on th FileField
+# def update_filename(instance, filename):
+# 	name_fill_space = instance.name.replace(' ','_')
+# 	name = name_fill_space + '/' + filename # potential space errors in name
+# 	return name
+
 class DataGroup(models.Model):
 
 	name = models.CharField(max_length=50)
@@ -14,7 +21,7 @@ class DataGroup(models.Model):
 	extraction_script = models.CharField(max_length=250, null=True, blank=True)
 	data_source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
 	updated_at = models.DateTimeField(default=timezone.now, null=True, blank=True)
-	csv = models.FileField(upload_to='csv/')
+	csv = models.FileField(upload_to='')
 
 	def __str__(self):
 		return self.name
@@ -24,62 +31,6 @@ class DataGroup(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('data_group_edit', kwargs={'pk': self.pk})
-
-# @login_required()
-# def register_upload(request, template='data_document/upload.html'):
-#     datagroup_pk = request.session['datagroup_pk']
-#     datagroup = DataGroup.objects.filter(pk=datagroup_pk)[0]
-#     context = {'datagroup_pk': datagroup_pk,}
-#     if request.method == 'POST' and request.FILES['myfile']:
-#         user = request.user.username + '_'
-#         myfile = request.FILES['myfile']
-#         fs = FileSystemStorage()
-#         fn = './{}/{}'.format(datagroup, myfile.name)
-#         filename = fs.save(fn, myfile)
-#         uploaded_file_url = fs.url(filename)
-#         context['uploaded_file_url'] = uploaded_file_url
-#         print(os.path.join(os.getcwd(), uploaded_file_url))
-#         print(os.getcwd())
-#         with open(uploaded_file_url) as csvfile:
-#             reader = csv.DictReader(csvfile)
-#             up_num = len([i for i,d in enumerate(reader) if d['filename']])
-#             valid = up_num == reader.line_num-1
-#         if not valid: # missing filenames, delete and prompt to fix
-#             context['uploaded_file_url'] = 0
-#             context['fail'] = (reader.line_num-1) -up_num
-#             fs.delete(os.path.join(os.getcwd(), uploaded_file_url)) # problems here, test
-#             return render(request, template, context)
-#         # if not DataDocument.objects.last():
-#         #     start = 0
-#         # else:
-#         #     start = DataDocument.objects.last().pk
-#         new_f = '/'.join(uploaded_file_url.split('/')[:-1]+[user + myfile.name])
-#         ordered_fieldnames = OrderedDict([('DataDocument_id',None),
-#                                             ('filename',None),
-#                                             ('title',None),
-#                                             ('product',None),
-#                                             ('url',None)])
-#         with open(uploaded_file_url, 'r') as csvfile:
-#             reader = csv.DictReader(csvfile)
-#             with open(new_f, 'w') as f:
-#                 writer = csv.DictWriter(f, fieldnames=ordered_fieldnames,
-#                                         lineterminator='\n')
-#                 writer.writeheader()
-#                 for row in reader:
-#                     print(row['filename'])
-#                     if not row['title']:
-#                         row['title'] = row['filename'].split('.')[0]
-#                     doc = DataDocument(filename=row['filename'],
-#                                         title=row['title'],
-#                                         url=row['url'],
-#                                         product_category=row['product'],
-#                                         data_group=datagroup)
-#                     doc.save()
-#                     row['DataDocument_id'] = doc.id
-#                     writer.writerow(row)
-#         fs.delete(os.path.join(os.getcwd(), uploaded_file_url))
-#         context['uploaded_num'] = up_num
-
 
 @receiver(models.signals.post_delete, sender=DataGroup)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
--- dashboard/views/data_group.py
+++ dashboard/views/data_group.py
@@ -5,6 +5,7 @@ from django.forms import ModelForm
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext_lazy as _
 from django.core.files import File
+from django.core.files.storage import FileSystemStorage
 
 from dashboard.views import *
 from dashboard.models import DataGroup, DataDocument
@@ -14,7 +15,7 @@ class DataGroupForm(ModelForm):
 		model = DataGroup
 		fields = ['name', 'description', 'downloaded_by', 'downloaded_at', 'extraction_script','data_source','updated_at','csv']
 		labels = {
-            'csv': _('Register Records'),
+            'csv': _('Register Records CSV File'),
         	}
 
 	def __init__(self, *args, **kwargs):
@@ -30,7 +31,8 @@ def data_group_list(request, template_name='data_group/datagroup_list.html'):
 
 
 @login_required()
-def data_group_detail(request, pk, template_name='data_group/datagroup_detail.html'):
+def data_group_detail(request, pk,
+						template_name='data_group/datagroup_detail.html'):
 	datagroup = get_object_or_404(DataGroup, pk=pk, )
 	docs = DataDocument.objects.filter(data_group_id=pk)
 	return render(request, template_name, {'object': datagroup,
@@ -44,49 +46,62 @@ def data_group_create(request, template_name='data_group/datagroup_form.html'):
 	datasource_title = request.session['datasource_title']
 	datasource_pk = request.session['datasource_pk']
 	# get the highest data group id number associated with the data source, increment by 1
-	new_group_key = DataGroup.objects.filter(data_source=datasource_pk).count() + 1
-	default_name = '{} {}'.format(datasource_title, new_group_key)
+	group_key = DataGroup.objects.filter(data_source=datasource_pk).count() + 1
+	default_name = '{} {}'.format(datasource_title, group_key)
 	initial_values = {'downloaded_by': request.user, 'name': default_name, 'data_source':datasource_pk}
 	form = DataGroupForm(request.POST or None, initial=initial_values)
 	if request.method == 'POST':
 		form = DataGroupForm(request.POST, request.FILES, user=request.user, \
 		 initial=initial_values)
 		if form.is_valid():
-			# Do something with the data
 			datagroup = form.save()
+			print(datagroup.pk)
+			dg_pk = datagroup.pk
 			info = datagroup.csv.readlines()
-			print(info[0])
 			text = ['DataDocument_id,' + info[0].decode('ascii')]
-			for line in info[1:]:
+			count = 0
+			for line in info[1:]: # read every csv line, create docs for each
+				count += 1
 				line = line.decode('ascii')
 				data = line.split(',')
+				if data[0] == '':
+					datagroup.csv.close()
+					datagroup.delete()
+					# count object tracks the row number w/o header
+					return render(request, template_name, {'file_check': count,
+															'form': form})
 				if data[1] == '': # updates title in line object
 					data[1] = data[0].split('.')[0]
-				doc=DataDocument(filename=data[0],title=data[1],product_category=data[2],url=data[3],data_group=datagroup)
+				doc=DataDocument(filename=data[0],
+								title=data[1],
+								product_category=data[2],
+								url=data[3],
+								data_group=datagroup)
 				doc.save()
+				# update line to hold the pk for writeout
 				text.append(str(doc.pk)+','+ ','.join(data))
 			with open(datagroup.csv.path,'w') as f:
 			    myfile = File(f)
 			    myfile.write(''.join(text))
-			#datagroup.register_csv()
+
 			return redirect('data_group_detail', pk=datagroup.id)
 	else:
 		form = DataGroupForm(user=request.user, initial=initial_values)
 	return render(request, template_name, {'form': form})
 
 @login_required()
-def data_group_update(request, pk, template_name='data_group/datagroup_form.html'):
+def data_group_update(request, pk):
 	datagroup = get_object_or_404(DataGroup, pk=pk)
 	form = DataGroupForm(request.POST or None, instance=datagroup)
 	if form.is_valid():
 		datagroup.updated_at = datetime.now()
 		form.save()
 		return redirect('data_group_list')
-	return render(request, template_name, {'form': form})
+	return render(request, 'data_group/datagroup_form.html', {'form': form})
 
 
 @login_required()
-def data_group_delete(request, pk, template_name='data_source/datagroup_confirm_delete.html'):
+def data_group_delete(request, pk, template_name='data_source/datasource_confirm_delete.html'):
 	datagroup = get_object_or_404(DataGroup, pk=pk)
 	if request.method == 'POST':
 		datagroup.delete()

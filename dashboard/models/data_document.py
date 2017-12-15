ccd6b9748fed0ba8c9379708ba50b2fb3049867f
dmlyons2 <dmlyons2@gmail.com>
2017-12-15 13:12:44 -0500

    Increased data document title length to 255

--- dashboard/models/data_document.py
+++ dashboard/models/data_document.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 class DataDocument(models.Model):
 
 	filename = models.CharField(max_length=100)
-	title = models.CharField(max_length=100)
+	title = models.CharField(max_length=255)
 	url = models.CharField(null=True, blank=True, max_length=200)
 	product_category = models.CharField(null=True, blank=True , max_length=50)
 	data_group = models.ForeignKey('DataGroup', on_delete=models.CASCADE)

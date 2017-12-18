1c097efc05f2b887a37be640c529b22063f576d3
rickD <debboutr@gmail.com>
2017-12-18 15:38:51 -0800

    add test with matching document

--- dashboard/tests/test_through_issue_30.py
+++ dashboard/tests/test_through_issue_30.py
@@ -8,10 +8,14 @@ from .testing_utilities import populate_test_db
 
 class TestThroughIssue30(TestCase):
     def setUp(self):
-        'Populate database'
+        # Populate database
         populate_test_db()
-    
+
     def test_data_group_matched_docs(self):
-        'Confirm that the DataGroup with no related documents returns zero for matched_docs'
-        dg = DataGroup.objects.filter(name='Test DG 1')[:1].get()
+        # Confirm that the DataGroup with no related documents returns zero for matched_docs
+        dg = DataGroup.objects.get(pk=1)
         self.assertEquals(dg.matched_docs(), 0)
+
+        # Confirm that the DataGroup WITH related documents returns greater than zero for matched_docs
+        dg = DataGroup.objects.get(pk=2)
+        self.assertGreater(dg.matched_docs(), 0)
--- dashboard/tests/testing_utilities.py
+++ dashboard/tests/testing_utilities.py
@@ -2,7 +2,7 @@ import json
 from django.test import TestCase, RequestFactory
 from dashboard.models import DataSource
 from dashboard.models import SourceType
-from dashboard.models import DataGroup
+from dashboard.models import DataGroup, DataDocument
 from django.contrib.auth.models import User
 from django.utils import timezone
 
@@ -18,16 +18,17 @@ def populate_test_db():
     SourceType.objects.create(title='Test SourceType')
 
     DataSource.objects.create(
-        title='Test Data Source', 
-        url="test_url.com", 
+        title='Test Data Source',
+        url="test_url.com",
         estimated_records=100,
         type=SourceType.objects.get(title='Test SourceType'),
         state='AT',
         description='a description of the data source',
         created_at='2017-12-18 20:09:49'
         )
-    
+
     DataGroup.objects.create(
+        pk = 1,
         name = 'Test DG 1',
         description = 'A test Data Group object',
         downloaded_by = User.objects.get(username='admin'),
@@ -36,4 +37,23 @@ def populate_test_db():
         data_source = DataSource.objects.get(pk=1),
         csv = '',
         zip_file = ''
+        )
+
+
+    DataGroup.objects.create(
+        pk = 2,
+        name = 'Test DG 2',
+        description = 'Another test Data Group object',
+        downloaded_by = User.objects.get(username='admin'),
+        downloaded_at = '2017-12-18 22:09:49',
+        extraction_script = 'script_name.rmd',
+        data_source = DataSource.objects.get(pk=1),
+        csv = '',
+        zip_file = ''
+        )
+
+    DataDocument.objects.create(
+        pk = 1,
+        matched = 1,
+        data_group = DataGroup.objects.get(pk=2),
     )

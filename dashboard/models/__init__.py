0d91e3e63e4db70f6df49ce1875d399998c2bbdb
dmlyons2 <dmlyons2@gmail.com>
2017-12-26 10:45:15 -0500

    Added product model

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -3,3 +3,4 @@ from .source_type import SourceType
 from .data_group import DataGroup
 from .data_document import DataDocument
 from .extraction_script import ExtractionScript
+from .product import Product

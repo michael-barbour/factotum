5a197b56c6c163425a84c599b4e695644f125eed
rickD <debboutr@gmail.com>
2018-01-09 00:00:26 -0800

    add datasource functional tests

--- dashboard/tests/functional_tests.py
+++ dashboard/tests/functional_tests.py
@@ -1,6 +1,12 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
+from django.test import LiveServerTestCase
+
+from dashboard.models import DataGroup
+
+val = len(DataGroup.objects.filter(data_source_id=1))
 URL = 'http://127.0.0.1:8000'
 
 def log_karyn_in(object):
@@ -31,7 +37,7 @@ class TestAuthInBrowser(unittest.TestCase):
 		body = self.browser.find_element_by_tag_name('body')
 		self.assertIn('Welcome to Factotum', body.text)
 
-class TestDataSource(unittest.TestCase):
+class TestDataSource(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -44,6 +50,35 @@ class TestDataSource(unittest.TestCase):
 		self.browser.get(URL + '/datasource/1')
 		h1 = self.browser.find_element_by_name('title')
 		self.assertIn('Walmart MSDS', h1.text)
+
+	#When a new data source is entered, the data source is automatically assigned the state 'awaiting triage.'
+	def test_state_and_priority(self):
+		valid_states = ['Awaiting Triage','In Progress','Complete','Stale']
+		valid_priorities = ['High','Medium','Low']
+		self.browser.get(URL + '/datasource/1')
+		state = self.browser.find_element_by_name('state')
+		self.assertIn(state.text, valid_states)
+		self.assertIn('Awaiting Triage', state.text)
+		select = Select(self.browser.find_element_by_name('priority'))
+		self.assertEqual([o.text for o in select.options], valid_priorities)
+		selected_option = select.first_selected_option
+		self.assertIn(selected_option.text, valid_priorities)
+		# is there a better way to loop through datasources???
+		# do we need to do all ????
+		self.browser.get(URL + '/datasource/2')
+		state = self.browser.find_element_by_name('state')
+		self.assertIn(state.text, valid_states)
+		self.assertIn('Awaiting Triage', state.text)
+
+	def test_datagroup_list_length(self):
+		b = len(DataGroup.objects.filter(data_source_id=1))
+		self.browser.get(URL + '/datasource/1')
+		row_count = len(self.browser.find_elements_by_xpath(
+								"//table[@id='data_group_table']/tbody/tr"))
+		# I'm getting vall from above, I can't seem to get the queryset to
+		# return inside of this function
+		self.assertEqual(val, row_count)
+
 
 class TestDataGroup(unittest.TestCase):
 

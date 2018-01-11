3c1e61f939f13acdd0c80c1e91d041f4074f9b06
Matt Frost <mwfrost@fastmail.fm>
2018-01-11 10:23:21 -0800

    Aded creation of new DataGroup to functional tests

--- dashboard/tests/functional_tests.py
+++ dashboard/tests/functional_tests.py
@@ -1,10 +1,15 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
-
+from django.utils import timezone
 from django.test import LiveServerTestCase
 
-from dashboard.models import DataGroup
+from dashboard.models import DataGroup, DataSource
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.storage import FileSystemStorage
+
 
 val = len(DataGroup.objects.filter(data_source_id=1))
 URL = 'http://127.0.0.1:8000'
@@ -38,6 +43,8 @@ class TestAuthInBrowser(unittest.TestCase):
 		self.assertIn('Welcome to Factotum', body.text)
 
 class TestDataSource(LiveServerTestCase):
+
+	fixtures = ['seed_data']
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -75,12 +82,12 @@ class TestDataSource(LiveServerTestCase):
 		self.browser.get(URL + '/datasource/1')
 		row_count = len(self.browser.find_elements_by_xpath(
 								"//table[@id='data_group_table']/tbody/tr"))
-		# I'm getting vall from above, I can't seem to get the queryset to
-		# return inside of this function
-		self.assertEqual(val, row_count)
+		self.assertEqual(b, row_count)
 
 
-class TestDataGroup(unittest.TestCase):
+class TestDataGroup(LiveServerTestCase):
+
+	fixtures = ['seed_data']
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -93,3 +100,64 @@ class TestDataGroup(unittest.TestCase):
 		self.browser.get(URL + '/datagroup/1')
 		h1 = self.browser.find_element_by_name('title')
 		self.assertIn('Walmart MSDS', h1.text)
+		pdflink = self.browser.find_elements_by_xpath('/html/body/div/table/tbody/tr[1]/td[1]/a')[0]
+		self.assertIn('shampoo.pdf',pdflink.get_attribute('href'))
+	
+	def create_data_group(self, data_source, testusername = 'Karyn', name='Walmart MSDS 3', description='Another data group, added programatically'):
+		source_csv = open('./sample_files/walmart_msds_3.csv','rb')
+		return DataGroup.objects.create(name=name, 
+										description=description, data_source = data_source,
+										downloaded_by=User.objects.get(username=testusername) ,
+										downloaded_at=timezone.now(),
+										csv=SimpleUploadedFile('walmart_msds_3.csv', source_csv.read() )
+										)
+	
+	def upload_pdfs(self):
+		store = settings.MEDIA_URL + self.dg.dgurl()
+		pdf1_name = '0ffeb4ae-2e36-4400-8cfc-a63611011d44.pdf'
+		pdf2_name = '1af3fa0f-edf3-4a41-bcec-7436d0689bd8.pdf'
+		local_pdf = open('./sample_files/' + pdf1_name, 'rb')
+		fs = FileSystemStorage(store + '/pdf')
+		fs.save(pdf1_name, local_pdf)
+		local_pdf = open('./sample_files/' + pdf2_name, 'rb')
+		fs = FileSystemStorage(store + '/pdf')
+		fs.save(pdf2_name, local_pdf)
+		return [pdf1_name, pdf2_name]
+
+	def create_data_documents(self, data_group):
+		dds = []
+		#pdfs = [f for f in os.listdir('/media/' + self.dg.dgurl() + '/pdf') if f.endswith('.pdf')]
+		#pdfs
+		with open(data_group.csv.path) as dg_csv:
+			table = csv.DictReader(dg_csv)
+			text = ['DataDocument_id,' + ','.join(table.fieldnames)+'\n']
+			errors = []
+			count = 0
+			for line in table: # read every csv line, create docs for each
+					count+=1
+					if line['filename'] == '':
+						errors.append(count)
+					if line['title'] == '': # updates title in line object
+						line['title'] = line['filename'].split('.')[0]
+					dd = DataDocument.objects.create(filename=line['filename'],
+						title=line['title'],
+						product_category=line['product'],
+						url=line['url'],
+						matched = line['filename'] in self.pdfs,
+						data_group=data_group)
+					dds.append(dd)
+			return dds
+
+	# creation of another DataGroup from csv and pdf sources
+	def test_new_data_group(self):
+		# DataGroup
+		dg_count_before = DataGroup.objects.count()
+		ds = DataSource.objects.get(pk=1)
+		self.dg = self.create_data_group(data_source=ds)
+		dg_count_after = DataGroup.objects.count()
+		self.assertEqual(dg_count_after, dg_count_before + 1) # the DataGroup object has been created
+		self.pdfs = self.upload_pdfs()
+		#self.dds = self.create_data_documents(self.dg)
+		
+		# deleting the DataGroup will clean up the file system
+		self.dg.delete()

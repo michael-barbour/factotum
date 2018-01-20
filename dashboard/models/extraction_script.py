12bea66a4a7c6d92cfd7d22e2e8097edef543897
Matt Frost <mwfrost@fastmail.fm>
2018-01-19 16:26:34 -0800

    correct tab/space problem

--- dashboard/models/extraction_script.py
+++ dashboard/models/extraction_script.py
@@ -6,14 +6,14 @@ from django.core.validators import URLValidator
 
 class ExtractionScript(models.Model):
 
-	title = models.CharField(max_length=50)
-	url = models.TextField(null=True, blank=True, validators=[URLValidator()])
+    title = models.CharField(max_length=50)
+    url = models.TextField(null=True, blank=True, validators=[URLValidator()])
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return reverse('extraction_script_edit', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse('extraction_script_edit', kwargs={'pk': self.pk})
 
     def get_datadocument_count(self):
         return self.datadocument_set.count()
@@ -22,4 +22,5 @@ class ExtractionScript(models.Model):
         return self.datadocument_set.filter(extraction_script=self.pk, extractedtext__qa_done=True).count()
 
     def get_pct_checked(self):
+        return 0
         

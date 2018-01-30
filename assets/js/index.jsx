5a5ae0cf2c5ab54fc6734fd2689965208e93745d
Zach <zach.munzenrider@csra.com>
2018-01-30 14:10:40 -0500

    adding webpack to project -- see wiki for instructions

---
+++ assets/js/index.jsx
@@ -0,0 +1,6 @@
+var jquery = require('jquery');
+var popper = require('popper.js');
+var bootstrap = require('bootstrap');
+var datatables = require('datatables');
+var d3 = require('d3');
+var nvd3 = require('nvd3');

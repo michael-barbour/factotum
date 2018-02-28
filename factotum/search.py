61362998191ca8ea8bb9a115ac7d2a2f1e4e43c7
Matt Frost <mwfrost@fastmail.fm>
2018-02-28 10:32:06 -0800

    Fully qualified imported models for search; used autopep8 to lint two files to pep8 standard

--- factotum/search.py
+++ factotum/search.py
@@ -2,7 +2,7 @@ from elasticsearch_dsl.connections import connections
 from elasticsearch_dsl import DocType, Text, Date
 from elasticsearch.helpers import bulk
 from elasticsearch import Elasticsearch
-from dashboard import models
+from dashboard.models import Product, DataDocument
 from django.http import JsonResponse
 from haystack.generic_views import FacetedSearchView as BaseFacetedSearchView
 from haystack.query import SearchQuerySet
@@ -14,18 +14,20 @@ connections.create_connection()
 
 
 def bulk_indexing():
-# TODO: get bulk indexing to work
+    # TODO: get bulk indexing to work
     ProductIndex.init()
     es = Elasticsearch()
-    bulk(client=es, actions=(b.indexing() for b in models.Product.objects.all().iterator()))
+    bulk(client=es, actions=(b.indexing()
+                             for b in Product.objects.all().iterator()))
     DataDocumentIndex.init()
-    bulk(client=es, actions=(b.indexing() for b in models.DataDocument.objects.all().iterator()))
+    bulk(client=es, actions=(b.indexing()
+                             for b in DataDocument.objects.all().iterator()))
+
 
 class FacetedSearchView(BaseFacetedSearchView):
 
     form_class = FacetedProductSearchForm
-    facet_fields = ['prod_cat', 'brand_name','facet_model_name']
+    facet_fields = ['prod_cat', 'brand_name', 'facet_model_name']
     template_name = 'search/facet_search.html'
     paginate_by = 10
     context_object_name = 'object_list'
-

60ad4772cd5e660cac67c7c5aa86644f6afb1fe0
rickD <debboutr@gmail.com>
2018-03-11 11:47:45 -0700

    add primary key in chem_text, new validation for doc_date

--- dashboard/models/extracted_text.py
+++ dashboard/models/extracted_text.py
@@ -6,7 +6,8 @@ from .script import Script
 
 class ExtractedText(models.Model):
 
-	data_document = models.ForeignKey(DataDocument, on_delete=models.CASCADE)
+	data_document = models.OneToOneField(DataDocument, on_delete=models.CASCADE,
+											primary_key=True)
 	record_type = models.CharField(max_length=50, null=True, blank=True)
 	prod_name = models.CharField(max_length=500, null=True, blank=True)
 	doc_date = models.CharField(max_length=10, null=True, blank=True)
@@ -20,3 +21,9 @@ class ExtractedText(models.Model):
 	def clean(self):
 		if self.doc_date[4] != '-' or self.doc_date[7] != '-':
 			raise ValidationError('Date format is off, should be  YYYY-MM-DD.')
+		try:
+			int(self.doc_date[:2])
+			int(self.doc_date[5:7])
+			int(self.doc_date[8:10])
+		except ValueError:
+			raise ValidationError("Not all characters are integers.")

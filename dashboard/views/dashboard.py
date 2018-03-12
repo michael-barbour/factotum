9df191ef65d1944cd4c88e747aa0f03a6a32115e
Zach <zach.munzenrider@csra.com>
2018-03-12 12:03:40 -0400

    make document stat query call more db-neutral

--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from dashboard.models import DataGroup, DataDocument, DataSource
+from dashboard.models import DataGroup, DataDocument, DataSource, Product
 from django.db.models import Count, F
 import datetime, time
 
+current_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+select_upload_date = {"upload_date": """date(uploaded_at)"""}
 
 @login_required(login_url='/login')
 def index(request):
 	stats = {}
-	stats['group_count'] = DataGroup.objects.count()
-	stats['source_count'] = DataSource.objects.count()
-	stats['document_count'] = DataDocument.objects.count()
+	stats['datagroup_count'] = DataGroup.objects.count()
+	stats['datasource_count'] = DataSource.objects.count()
+	stats['datadocument_count'] = DataDocument.objects.count()
+	stats['product_count'] = Product.objects.count()
+	stats['product_with_puc_count'] = Product.objects.count()
+	stats['documents_uploaded_count_by_date'] = document_uploaded_count_by_date()
+	stats['document_percent_with_extracted_text_count'] = Product.objects.count()
 
-	# Build datasets to populate linechart with document-upload statistics
-	select_data = {"upload_date": """date_format(uploaded_at, '%%Y-%%m-%%d')"""}
-	# current_date = time.mktime(datetime.datetime.now().date().timetuple())
-	current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+	return render(request, 'dashboard/index.html', stats)
+
+def document_uploaded_count_by_date():
+	# Datasets to populate linechart with document-upload statistics
+	# Number of datadocuments, both overall and by type, that have been uploaded as of each date
 	document_stats = {}
-	document_stats['all'] = list(DataDocument.objects.extra(select=select_data) \
-								   .values('upload_date') \
-								   .annotate(document_count = Count('id')) \
-								   .order_by('upload_date'))
-	document_stats_by_type = DataDocument.objects.extra(select=select_data) \
+	document_stats['all'] = list(DataDocument.objects.extra(select=select_upload_date) \
+								 .values('upload_date') \
+								 .annotate(document_count = Count('id')) \
+								 .order_by('upload_date'))
+	document_stats_by_type = DataDocument.objects.extra(select=select_upload_date) \
 		.values('upload_date') \
 		.annotate(source_type = F('data_group__data_source__type__title'), document_count = Count('id')) \
 		.order_by('upload_date')
@@ -30,13 +37,12 @@ def index(request):
 	for type in {'all', 'product', 'msds_sds'}:
 		document_count = 0
 		for item in document_stats[type]:
+			item['upload_date'] = datetime.date.strftime((item['upload_date']), '%Y-%m-%d')
 			document_count += item['document_count']
 			item['document_count'] = document_count
 		# if final record isn't for current date, create one
 		for item in document_stats[type][len(document_stats[type])-1:]:
 			if item['upload_date'] != current_date:
 				document_stats[type].append({'upload_date': current_date
-										  , 'document_count': document_count})
-	stats['document_stats'] = document_stats
-
-	return render(request, 'dashboard/index.html', stats)
+												, 'document_count': document_count})
+	return document_stats

47252f57e8acd3f96a66514ef3ba988c985aabf8
Matt Frost <mwfrost@fastmail.fm>
2018-03-21 14:42:03 -0700

    Remove the DataGroup model's default foreign key to Script

--- dashboard/models/data_group.py
+++ dashboard/models/data_group.py
@@ -21,7 +21,7 @@ class DataGroup(models.Model):
 	description = models.TextField(null=True, blank=True)
 	downloaded_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	downloaded_at = models.DateTimeField()
-	download_script = models.ForeignKey('Script', on_delete=models.CASCADE, default=1, null=True, blank=True)
+	download_script = models.ForeignKey('Script', on_delete=models.CASCADE, default=None, null=True, blank=True)
 	data_source = models.ForeignKey('DataSource', on_delete=models.CASCADE)
 	updated_at = models.DateTimeField(default=timezone.now, null=True, blank=True)
 	csv = models.FileField(upload_to=update_filename, null=True)
--- dashboard/tests/functional_tests.py
+++ dashboard/tests/functional_tests.py
@@ -99,9 +99,9 @@ class TestDataSourceAndDataGroup(StaticLiveServerTestCase):
     # Data Groups
 
     def test_data_group_name(self):
-        self.browser.get(self.live_server_url + '/datagroup/1')
+        self.browser.get(self.live_server_url + '/datagroup/40')
         h1 = self.browser.find_element_by_name('title')
-        self.assertIn('Walmart MSDS', h1.text)
+        self.assertIn('Walmart MSDS 1', h1.text)
         # Checking the URL by row is too brittle
         #pdflink = self.browser.find_elements_by_xpath(
         #    '//*[@id="d-docs"]/tbody/tr[2]/td[1]/a')[0]
@@ -113,10 +113,12 @@ class TestDataSourceAndDataGroup(StaticLiveServerTestCase):
                                        'added programatically')):
         source_csv = open('./sample_files/walmart_msds_3.csv', 'rb')
         return DataGroup.objects.create(name=name,
-                                        description=description, data_source=data_source,
+                                        description=description, 
+                                        data_source=data_source,
                                         downloaded_by=User.objects.get(
                                             username=testusername),
                                         downloaded_at=timezone.now(),
+                                        download_script=None,
                                         csv=SimpleUploadedFile('walmart_msds_3.csv', source_csv.read()))
 
     def upload_pdfs(self):
@@ -158,20 +160,19 @@ class TestDataSourceAndDataGroup(StaticLiveServerTestCase):
     def test_new_data_group(self):
         # DataGroup, created using the model layer
         dg_count_before = DataGroup.objects.count()
-        ds = DataSource.objects.get(pk=1)
+        ds = DataSource.objects.get(pk=2)
         self.dg = self.create_data_group(data_source=ds)
         dg_count_after = DataGroup.objects.count()
         self.assertEqual(dg_count_after, dg_count_before + 1,
                          "Confirm the DataGroup object has been created")
-        self.assertEqual(3, self.dg.pk,
-                         "Confirm the new DataGroup object's pk is 3")
+        new_dg_pk = self.dg.pk
         self.pdfs = self.upload_pdfs()
         self.dds = self.create_data_documents(self.dg, ds)
 
         # Use the browser layer to confirm that the object has been created
-        self.browser.get('%s%s' % (self.live_server_url, '/datagroup/3'))
+        self.browser.get('%s%s%s' % (self.live_server_url, '/datagroup/', new_dg_pk))
         self.assertEqual('factotum', self.browser.title,
-                         "Testing open of datagroup 3 show page")
+                         "Testing open of show page for new data group")
 
         self.browser.get(self.live_server_url + reverse('data_group_detail',
                                                         kwargs={'pk': self.dg.pk}))
@@ -181,7 +182,7 @@ class TestDataSourceAndDataGroup(StaticLiveServerTestCase):
 
         # Use the browser layer to delete the DataGroup object
         # deleting the DataGroup should clean up the file system
-        self.browser.get(self.live_server_url + '/datagroup/delete/3')
+        self.browser.get('%s%s%s' % (self.live_server_url, '/datagroup/delete/', new_dg_pk))
         del_button = self.browser.find_elements_by_xpath(('/html/body/div/form/'
                                                           'input[2]'))[0]
         del_button.click()
@@ -189,16 +190,17 @@ class TestDataSourceAndDataGroup(StaticLiveServerTestCase):
                          "Confirm the DataGroup object has been deleted")
 
     def test_pagination(self):
+        # The data group detail page uses server-side pagination to display the 
+        # related data documents
         self.browser.get('%s%s' % (self.live_server_url, '/datagroup/40'))
-        h1 = self.browser.find_element_by_name('title')
-        self.assertEqual('Walmart MSDS 1', h1.text)
+        pagebox = self.browser.find_elements_by_class_name("current")[0]
+        self.assertIn('Page 1', pagebox.text,
+                         "Confirm the current page navigation box displays 'Page 1 of [some number]'")
+        nextbox = self.browser.find_element_by_xpath('/html/body/div[1]/nav/ul/li[2]/a')
+        nextbox.click()
+        self.assertIn("?page=2", self.browser.current_url,
+                      'The newly opened URL should contain the new page number')
 
-        #pagebox = self.browser.find_elements_by_class_name("current")[0]
-        pagebox = self.browser.find_elements_by_xpath(('/html/body/div[1]/nav/ul/li[1]/span'))[0]
-        self.assertEqual(pagebox.text, 'Page 1 of 50',
-                         "Confirm the navigation box displays 'Page 1 of 50'")
-        
-        
 
 
 class TestProductCuration(StaticLiveServerTestCase):

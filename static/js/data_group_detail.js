6c8af47bddbb9645f319e1b09b6a131d261637e3
rickD <debboutr@gmail.com>
2018-03-29 12:11:21 -0700

    registered_records downloading from hidden table

--- static/js/data_group_detail.js
+++ static/js/data_group_detail.js
@@ -17,37 +17,43 @@
     });
 
     function exportTableToCSV($table, filename) {
-          var $rows = $table.find('tr:has(td),tr:has(th)'),
-              // Temporary delimiter characters unlikely to be typed by keyboard
-              // This is to avoid accidentally splitting the actual contents
-              tmpColDelim = String.fromCharCode(11), // vertical tab character
-              tmpRowDelim = String.fromCharCode(0), // null character
-              // actual delimiter characters for CSV format
-              colDelim = '","',
-              rowDelim = '"\r\n"',
-              // Grab text from table into CSV formatted string
-              csv = '"' + $rows.map(function (i, row) {
-                  var $row = $(row), $cols = $row.find('td,th');
-                  return $cols.map(function (j, col) {
-                      var $col = $(col), text = $col.text();
-                      return text.replace(/"/g, '""'); // escape double quotes
-                  }).get().join(tmpColDelim);
-              }).get().join(tmpRowDelim)
-                  .split(tmpRowDelim).join(rowDelim)
-                  .split(tmpColDelim).join(colDelim) + '"',
-              csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
-            if (window.navigator.msSaveBlob) { // IE 10+
-              window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
-            }
-            else {
-              $(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' });
-            }
+      var $rows = $table.find('tr:has(td),tr:has(th)'),
+          // Temporary delimiter characters unlikely to be typed by keyboard
+          // This is to avoid accidentally splitting the actual contents
+          tmpColDelim = String.fromCharCode(11), // vertical tab character
+          tmpRowDelim = String.fromCharCode(0), // null character
+          // actual delimiter characters for CSV format
+          colDelim = '","',
+          rowDelim = '"\r\n"',
+          // Grab text from table into CSV formatted string
+          csv = '"' + $rows.map(function (i, row) {
+              var $row = $(row), $cols = $row.find('td,th');
+              return $cols.map(function (j, col) {
+                  var $col = $(col), text = $col.text();
+                  return text.replace(/"/g, '""'); // escape double quotes
+              }).get().join(tmpColDelim);
+          }).get().join(tmpRowDelim)
+              .split(tmpRowDelim).join(rowDelim)
+              .split(tmpColDelim).join(colDelim) + '"',
+          csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+        if (window.navigator.msSaveBlob) { // IE 10+
+          window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
+        }
+        else {
+          $(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' });
+        }
       }
-      
+
+    var dg_name = document.getElementById("dg_name").value;
       // This must be a hyperlink
-    $("#xx").on('click', function (event) {
-        exportTableToCSV.apply(this, [$('#extract'), 'export.csv']);
+    $("#rr").on('click', function (event) {
+        exportTableToCSV.apply(this, [$('#registered'), dg_name + '_registered_records.csv']);
     });
+
+    $("#xx").on('click', function (event) {
+        exportTableToCSV.apply(this, [$('#extract'), dg_name + '_extract_template.csv']);
+    });
+
     // make submit button active when file selected
     $('input:file').change(
         function(){

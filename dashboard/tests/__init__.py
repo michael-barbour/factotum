6d6ba84cc4fa12f3e094d103c0394a377529edca
Matt Frost <mwfrost@fastmail.fm>
2018-04-24 07:35:03 -0700

    Change name of test file; edit init file

--- dashboard/tests/__init__.py
+++ dashboard/tests/__init__.py
@@ -2,4 +2,6 @@
 # from .test_auth import *
 from .test_models import *
 # from .test_templates import *
-from .functional_tests import *
+# from .functional_tests import *
+from .functional_tests_rollback import *
+

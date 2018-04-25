a4e163b106e33922f93b300820eba6c785f6b6fd
Matt Frost <mwfrost@fastmail.fm>
2018-04-25 10:07:18 -0700

    Remove commented tests from __init__.py

--- dashboard/tests/__init__.py
+++ dashboard/tests/__init__.py
@@ -1,7 +1,4 @@
 # new test modules need to be added here.
-# from .test_auth import *
 from .test_models import *
-# from .test_templates import *
-# from .functional_tests import *
 from .functional_tests_rollback import *
 

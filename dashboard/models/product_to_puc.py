ce8bd2b20569f6ea7723c20563c163ebe2f82d1c
daveahern <david.ahern@csra.com>
2018-05-22 11:49:32 -0400

    Application changes to account for new product_to_puc linker table.  Still incomplete, two issues need to be addressed.

--- dashboard/models/product_to_puc.py
+++ dashboard/models/product_to_puc.py
@@ -1,7 +1,7 @@
 from django.db import models
 from .common_info import CommonInfo
 from .product import Product
-from .product_category import ProductCategory
+from .PUC import PUC
 
 
 class ProductToPUC(CommonInfo):
@@ -12,14 +12,14 @@ class ProductToPUC(CommonInfo):
         (AUTOMATIC, 'Automatic'))
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    PUC = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    PUC = models.ForeignKey(PUC, on_delete=models.CASCADE)
     puc_assigned_usr = models.ForeignKey('auth.User',
                                          on_delete=models.SET_NULL, null=True, blank=True)
     puc_assigned_script = models.ForeignKey('Script',
                                             on_delete=models.SET_NULL, null=True, blank=True)
     puc_assigned_time = models.DateTimeField(null=True, blank=True)
-    classification_method = models.CharField(max_length=2, choices=CLASSIFICATION_CHOICES, null=False, default='AU')
-    classification_confidence = models.DecimalField(max_digits=6, decimal_places=3, null=True, blank=True)
+    classification_method = models.CharField(max_length=2, choices=CLASSIFICATION_CHOICES, null=False, default='MA')
+    classification_confidence = models.DecimalField(max_digits=6, decimal_places=3, default=1, null=True, blank=True)
 
     def __str__(self):
         return self.id

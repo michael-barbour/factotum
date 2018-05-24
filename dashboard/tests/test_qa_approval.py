7f68226ae05dcd6cf5ae18efeb22d2bd0051896b
Matt Frost <mwfrost@fastmail.fm>
2018-05-24 14:44:57 -0700

    Delete unused test case; add unit tests for testing the QA screen

--- dashboard/tests/test_qa_approval.py
+++ dashboard/tests/test_qa_approval.py
@@ -1,18 +1,43 @@
 from django.test import Client
-from django.test import TestCase, override_settings
+from django.test import TestCase, override_settings, RequestFactory
+from dashboard.models import DataDocument, Script, ExtractedText, ExtractedChemical, QAGroup
 
+@override_settings(ALLOWED_HOSTS=['testserver'])
 class TestApprovalView(TestCase):
     fixtures = ['00_superuser.yaml','01_lookups.yaml',
-                '02_datasource.yaml','03_datagroup.yaml',
-                '04_productcategory.yaml','05_product_lite.yaml',
-                '06_datadocument_lite.yaml','07_script.yaml',
-                '08_extractedtext_lite.yaml','09_productdocument_lite.yaml',
-                '10_extractedchemical.yaml']
-    @override_settings(ALLOWED_HOSTS=['testserver'])
-    def test_other_domain(self):
-        response = self.client.post('/login/', {'username': 'karyn', 'password': 'specialP@55word'})
-        print(response.status_code)
-        print(response)
+    '02_datasource.yaml','03_datagroup.yaml',
+    '04_productcategory.yaml','05_product_lite.yaml',
+    '06_datadocument_lite.yaml','07_script.yaml',
+    '08_extractedtext_lite.yaml','09_productdocument_lite.yaml',
+    '10_extractedchemical.yaml']
+    def setUp(self):
+        print('setUp running')
+        self.factory = RequestFactory()
+        self.client.login(username='karyn', password='specialP@55word')
+
+
+    def test_qa_begin(self):
+        """
+        Check that starting the QA process flips the variable on the Script
+        """
+        self.assertTrue( not Script.objects.get(pk=5).qa_begun, 
+        'The Script should have qa_begun of False at the beginning')
+        response = self.client.get('/qa/extractionscript/5', follow=True)
+        self.assertTrue(Script.objects.get(pk=5).qa_begun, 
+        'qa_begun should now be true')
+        #self.assertIn(b'<title>factotum</title>', response.content)
+        
+
+    def test_approval(self):
+        # Find and follow the first approval link
+        response = self.client.get('/qa/extractionscript/5', follow=True)
+        response = self.client.get('/qa/extractedtext/7', follow=True)
+        
+        
+
+
+
+
     
 
 

63e2ee4044bf0e19e86d2d071ed2f1c1f3b4fdd6
Dave Lyons <dmlyons2@gmail.com>
2018-06-04 08:40:46 -0400

    migration and added PUCAttribute to fixtures

--- dashboard/models/PUC.py
+++ dashboard/models/PUC.py
@@ -8,9 +8,9 @@ class PUC(CommonInfo):
     prod_fam = models.CharField(max_length=50, null=True, blank=True)
     prod_type = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=False, blank=False)
-    last_edited_by = models.ForeignKey('auth.User', default = 1, on_delete=models.CASCADE)
+    last_edited_by = models.ForeignKey('auth.User', default=1, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='ProductToPUC')
-    attribute = models.ForeignKey('PUCAttribute', on_delete=models.PROTECT)
+    attribute = models.ForeignKey('PUCAttribute', on_delete=models.PROTECT, null=True, blank=True)
 
     class Meta:
         ordering = ['gen_cat', 'prod_fam', 'prod_type']
@@ -19,7 +19,7 @@ class PUC(CommonInfo):
     def __str__(self):
         cats = (self.gen_cat, ' - ', self.prod_fam, ' - ', self.prod_type)
         return ''.join(str(cat) for cat in cats if cat is not None)
-        #return self.gen_cat + ' - ' + self.prod_fam + ' - ' + self.prod_type # this may need to change
+        # return self.gen_cat + ' - ' + self.prod_fam + ' - ' + self.prod_type # this may need to change
 
     def natural_key(self):
         return self.gen_cat

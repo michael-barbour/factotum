b6ff7bc6216f594e7f243c8d007fb0120e9745c2
Matt Frost <Matthew.Frost@csra.com>
2018-06-20 14:08:35 -0400

    Updated more seed data

--- dashboard/fixtures/groom_seed_data.py
+++ dashboard/fixtures/groom_seed_data.py
@@ -7,19 +7,15 @@ python manage.py loaddata 00_superuser
 """
 
 from django.contrib.auth.models import User
-from dashboard.models import DataSource, DataGroup, DataDocument, ProductDocument, Product, ProductCategory
+from dashboard.models import DataSource, DataGroup, DataDocument, ProductDocument, Product, PUC, ProductToPUC, GroupType, DocumentType
 from django.db.models import Count
 
 # prep the objects by switching their related user to Karyn
 karyn = User.objects.get(username='Karyn')
 
 DataGroup.objects.values('downloaded_by__username').annotate(n=Count('id'))
-Product.objects.values('puc_assigned_usr_id__username').annotate(n=Count('id'))
 
 DataGroup.objects.all().update(downloaded_by=karyn)
-Product.objects.all().update(puc_assigned_usr_id=karyn)
-
-ProductCategory.objects.all().update(last_edited_by=karyn)
 
 # Remove all the real-world users
 User.objects.exclude(username='Karyn').delete()
@@ -36,12 +32,13 @@ DataGroup.objects.filter(id__in=[19,23,24]).delete()
 ######## At the command prompt:
 
 """ 
-python manage.py dumpdata dashboard.sourcetype --format=yaml > ./dashboard/fixtures/01_lookups.yaml
+python manage.py dumpdata dashboard.grouptype dashboard.documenttype dashboard.unittype dashboard.weightfractiontype dashboard.pucattribute --format=yaml > ./dashboard/fixtures/01_lookups.yaml
 python manage.py dumpdata dashboard.datasource --format=yaml > ./dashboard/fixtures/02_datasource.yaml
 python manage.py dumpdata dashboard.datagroup --format=yaml > ./dashboard/fixtures/03_datagroup.yaml
-python manage.py dumpdata dashboard.productcategory --format=yaml > ./dashboard/fixtures/04_productcategory.yaml
+python manage.py dumpdata dashboard.PUC --format=yaml > ./dashboard/fixtures/04_PUC.yaml
 python manage.py dumpdata dashboard.product --format=yaml > ./dashboard/fixtures/05_product.yaml
 python manage.py dumpdata dashboard.datadocument --format=yaml > ./dashboard/fixtures/06_datadocument.yaml
+python manage.py dumpdata dashboard.qagroup --format=yaml > ./dashboard/fixtures/065_qagroup.yaml
 python manage.py dumpdata dashboard.script --format=yaml > ./dashboard/fixtures/07_script.yaml
 python manage.py dumpdata dashboard.extractedtext --format=yaml > ./dashboard/fixtures/08_extractedtext.yaml
 python manage.py dumpdata dashboard.productdocument --format=yaml > ./dashboard/fixtures/09_productdocument.yaml

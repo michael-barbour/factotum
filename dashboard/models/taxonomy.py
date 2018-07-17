d717cda9bb086603d5570752324c7fd25b0e3454
Zach <zach.munzenrider@csra.com>
2018-07-17 09:43:39 -0400

    replacing tabs with spaces in taxonomy models

--- dashboard/models/taxonomy.py
+++ dashboard/models/taxonomy.py
@@ -3,19 +3,16 @@ from .common_info import CommonInfo
 
 
 class Taxonomy(CommonInfo):
-	title = models.CharField(max_length=100, blank=False, null=False)
-	description = models.TextField(null=True, blank=True)
-	parent = models.ForeignKey('Taxonomy', null=True, blank=True, on_delete=models.PROTECT)
-	source = models.ForeignKey('TaxonomySource', on_delete=models.PROTECT)
-	category_code = models.CharField(max_length=40, blank=True, null=True)
-	last_edited_by = models.ForeignKey('auth.User', default=1, on_delete=models.CASCADE)
-	product_category = models.ManyToManyField('PUC', through='TaxonomyToPUC')
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey('Taxonomy', null=True, blank=True, on_delete=models.PROTECT)
+    source = models.ForeignKey('TaxonomySource', on_delete=models.PROTECT)
+    category_code = models.CharField(max_length=40, blank=True, null=True)
+    last_edited_by = models.ForeignKey('auth.User', default=1, on_delete=models.CASCADE)
+    product_category = models.ManyToManyField('PUC', through='TaxonomyToPUC')
 
-	class Meta:
-		verbose_name_plural = 'Taxonomies'
+    class Meta:
+        verbose_name_plural = 'Taxonomies'
 
-	def __str__(self):
-		return str(self.title)
-
-
-
+    def __str__(self):
+        return str(self.title)

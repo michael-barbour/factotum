42de7933922451a60915a683a238c7a208e7e0da
daveahern <david.ahern@csra.com>
2018-07-23 10:22:03 -0400

    Resized extracted_text.doc_date to 25 chars. Removed validation for all but data length. Modified tests to test for only data length.

--- dashboard/tests/test_extracted_text.py
+++ dashboard/tests/test_extracted_text.py
@@ -14,26 +14,20 @@ class ExtractedTest(TestCase):
         self.client.login(username='Karyn', password='specialP@55word')
 
     def test_extracted_doc_date_validation(self):
-        # check validation for year
-        text = ExtractedText(doc_date= '2027-04-13',
-                                data_document=self.objects.doc,
-                                extraction_script=self.objects.script)
-        self.assertRaises(ValidationError, text.clean)
-        # check validation for month
-        text = ExtractedText(doc_date= '2010-24-13',
-                                data_document=self.objects.doc,
-                                extraction_script=self.objects.script)
-        self.assertRaises(ValidationError, text.clean)
-        # check validation for day
-        text = ExtractedText(doc_date= '2010-04-47',
-                                data_document=self.objects.doc,
-                                extraction_script=self.objects.script)
-        self.assertRaises(ValidationError, text.clean)
         # check validation for proper length string
-        text = ExtractedText(doc_date= '2010-04-1300',
+        text = ExtractedText(doc_date= 'Wednesday, January 21, 2014',
                                 data_document=self.objects.doc,
                                 extraction_script=self.objects.script)
         self.assertRaises(ValidationError, text.clean)
+        # check validation not thrown for arbitrary date string less than 25 chars
+        text = ExtractedText(doc_date= 'January 1984',
+                             data_document=self.objects.doc,
+                             extraction_script=self.objects.script)
+        try:
+            text.clean()
+        except ValidationError:
+            self.fail("clean() raised ExceptionType unexpectedly!")
+
         # check validation not thrown if doc_date is null
         text = ExtractedText(data_document=self.objects.doc,
                                 extraction_script=self.objects.script)

2cf1f764aee68e65f69d8af0d005951147054e75
daveahern <david.ahern@csra.com>
2018-07-26 15:36:02 -0400

    Re-adding text_extracted_qa and test_qa_approval to our testing stack.  Removing explicit tests from __init__ files in subf-folders.

--- dashboard/tests/unit/__init__.py
+++
@@ -1,5 +0,0 @@
-from .test_models import *
-from .test_extracted_functionaluse import *
-from .test_extracted_text import *
-from .test_product_to_puc import *
-from .test_product_to_attribute import *

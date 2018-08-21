4291f6b3590b34dfc0b8e1954297df8b721593d1
Matt Frost <mwfrost@fastmail.fm>
2018-08-20 21:50:36 -0700

    Updated queries in get_data page; built new tests

--- dashboard/tests/functional/test_get_data.py
+++ dashboard/tests/functional/test_get_data.py
@@ -6,6 +6,7 @@ from dashboard import views
 
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from dashboard.models import PUC, Product, ProductToPUC
 from dashboard.views.get_data import * 
 
 class TestGetData(TestCase):
@@ -18,12 +19,32 @@ class TestGetData(TestCase):
         self.client = Client()
 
     def test_dtxsid_stats(self):
-        ids =["DTXSID9022528", "DTXSID1020273","DTXSID6026296","DTXSID2021781"]
-        #stats = stats_by_dtxsids(ids)
-        #print(stats)
+        dtxs =["DTXSID9022528", "DTXSID1020273","DTXSID6026296","DTXSID2021781"]
+        # Functional test: the stats calculation
+        stats = stats_by_dtxsids(dtxs)
+        print(stats)
+        # select out the stats for one DTXSID, ethylparaben
+        ethylparaben_stats = stats.get(sid='DTXSID9022528')
+        self.assertEqual(0, ethylparaben_stats['pucs_n'])
+        # add a puc to one of the products containing ethylparaben
+        ps = DSSToxSubstance.objects.filter(sid='DTXSID9022528').\
+        values('extracted_chemical__extracted_text__data_document__products')
+        print('----- ps')
+        print(ps)
+        # get one of the products that is already linked to a data document with DTXSID9022528 in its extracted chemicals
+        p = ps.first()
+        puc = PUC.objects.get(id=20)
+        ppuc = ProductToPUC.objects.create(product=p,
+                                        PUC=puc,
+                                        puc_assigned_usr=User.objects.get(username='karyn'))
+        stats = stats_by_dtxsids(dtxs)
+        print(stats)
+        # select out the stats for one DTXSID, ethylparaben
+        ethylparaben_stats = stats.get(sid='DTXSID9022528')
+        self.assertEqual(1, ethylparaben_stats['pucs_n'])
+
         #csv_out = download_chem_stats(stats)
         #print(csv_out.content)
-        print("This test needs to be updated")
 
 
 

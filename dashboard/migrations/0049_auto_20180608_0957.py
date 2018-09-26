94b740c4bb5ae97880f46c83eba38298535c25fd
Matt Frost <mwfrost@fastmail.fm>
2018-09-26 13:33:52 -0700

    Create new migration 0066; edit old migration 0045 to add DataGroup object (including default record) later in migration chain

--- dashboard/migrations/0049_auto_20180608_0957.py
+++ dashboard/migrations/0049_auto_20180608_0957.py
@@ -7,24 +7,6 @@ from dashboard.models.group_type import GroupType
 from dashboard.models.document_type import DocumentType
 
 
-def create_default_data_group_type(apps, schema_editor):
-    # create the default "unidentified" group_type
-    GroupType.objects.create(title='Unidentified', description='Unidentified Group Type')
-
-    data_group = apps.get_model('dashboard', 'DataGroup')
-    for dg in data_group.objects.all():
-        dg.group_type_id = 1
-        dg.save()
-
-
-def create_default_document_type(apps, schema_editor):
-    # create the default "unidentified" document_type
-    DocumentType.objects.create(title='Unidentified', description='Unidentified Document Type', group_type_id=1)
-
-    docs = apps.get_model('dashboard', 'DataDocument')
-    for dd in docs.objects.all():
-        dd.document_type_id = 1
-        dd.save()
 
 
 class Migration(migrations.Migration):
@@ -33,6 +15,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_default_data_group_type),
-        migrations.RunPython(create_default_document_type),
+       
     ]

5d5a0ce06665b096f1beac054eb15d929f0c2ece
wmelende <melendez.wilson@epa.gov>
2018-10-03 13:42:18 -0500

    Started pseudo-code by adding comments to the script.

--- factotum/scripts/product_dup_check.py
+++ factotum/scripts/product_dup_check.py
@@ -1,1 +1,14 @@
-# Test
+# Get all the products that have stub UPCs.
+
+# Create a variable that will have all products that have non-stub UPCs.
+
+# Loop through stubs
+
+## Find title and compare it to every non-stub product.
+
+## Determine whether or not there was a match.
+
+### There was a match
+
+
+### There was no match

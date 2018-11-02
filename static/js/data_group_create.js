00b2fbd2ceac52ab968e8d7016e89e4936bab177
rickD <debboutr@gmail.com>
2018-11-02 11:36:35 -0700

    move code tables to nav bar

--- static/js/data_group_create.js
+++ static/js/data_group_create.js
@@ -1,12 +1,5 @@
   $(document).ready(function () {
-      var stickyHeaderTop = $('#accordion').offset().top;
-      // var stickyHeaderTop = $('#nav').offset().bottom;
-      console.log(stickyHeaderTop);
-      $(window).scroll(function(){
-              if( $(window).scrollTop() > stickyHeaderTop -47) {
-                      $('#accordion').css({position: 'fixed', top: '40px'});
-              } else {
-                      $('#accordion').css({position: 'static', top: '0px'});
-              }
+      $(document).on('click', '.allow-focus .dropdown-menu', function (e) {
+        e.stopPropagation();
       });
     });

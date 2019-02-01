5ebfdfe13144882b8f823f9019e58a9794f43fa3
Matt Frost <mwfrost@fastmail.fm>
2019-02-01 14:32:24 -0800

    Modify get_data queries for new schema
    All tests except dashboard.tests.functional.test_extracted_text_upload.UploadExtractedFileTest are now passing

--- dashboard/tests/functional/test_get_data.py
+++ dashboard/tests/functional/test_get_data.py
@@ -36,8 +36,8 @@ class TestGetData(TestCase):
 
         self.client.login(username='Karyn', password='specialP@55word')
         # get the associated documents for linking to products
-        dds = DataDocument.objects.filter(pk__in=DSSToxSubstance.objects.filter(sid='DTXSID9022528').\
-        values('rawchem_ptr__extracted_chemical__extracted_text__data_document'))
+        dds = DataDocument.objects.filter(pk__in=ExtractedChemical.objects.filter(dsstox__sid='DTXSID9022528').\
+        values('extracted_text__data_document'))
         dd = dds[0]
 
         ds = dd.data_group.data_source
@@ -73,8 +73,9 @@ class TestGetData(TestCase):
         # change the number of related data documents by deleting one
         self.client.login(username='Karyn', password='specialP@55word')
         # get the associated documents for linking to products
-        dds = DataDocument.objects.filter(pk__in=DSSToxSubstance.objects.filter(sid='DTXSID9022528').\
-            values('rawchem_ptr__extracted_chemical__extracted_text__data_document'))
+        dds = DataDocument.objects.filter(pk__in=ExtractedChemical.objects.filter(dsstox__sid='DTXSID9022528').\
+            values('extracted_text__data_document'))
+
         dd = dds[0]
         dd.delete()
 
@@ -121,8 +122,8 @@ class TestGetData(TestCase):
         associated with ethylparaben')
         self.client.login(username='Karyn', password='specialP@55word')
         # get the associated documents for linking to products
-        dds = DataDocument.objects.filter(pk__in=DSSToxSubstance.objects.filter(sid='DTXSID9022528').\
-        values('rawchem_ptr__extracted_chemical__extracted_text__data_document'))
+        dds = DataDocument.objects.filter(pk__in=ExtractedChemical.objects.filter(dsstox__sid='DTXSID9022528').\
+        values('extracted_text__data_document'))
         dd = dds[0]
 
 

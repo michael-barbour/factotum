dacf0bf47c661eee915cbed21759d61879fa424f
Matt Frost <mwfrost@fastmail.fm>
2019-02-08 09:45:32 -0800

    Remove save button, adjust order of fixture loading

--- dashboard/fixtures/load_fixtures.sh
+++ dashboard/fixtures/load_fixtures.sh
@@ -3,14 +3,14 @@ python manage.py flush
 python manage.py makemigrations
 python manage.py migrate
 # specialP@55word
-python manage.py loaddata 00_superuser 01_lookups \
-    02_datasource 03_datagroup 04_PUC 05_product \
-    06_datadocument 07_script 08_extractedtext \
-    09_productdocument 10_extractedchemical \
-    11_dsstoxsubstance 12_habits_and_practices\
-    13_habits_and_practices_to_puc 14_product_to_puc\
-    15_extractedfunctionaluse 16_extractedcpcat\
-    17_extractedlistpresence 18_puc_tag
+python manage.py loaddata 00_superuser \
+    01_lookups 02_datasource 03_datagroup \
+    04_PUC 05_product 06_datadocument  \
+    07_script 08_extractedtext 16_extractedcpcat \
+    09_productdocument 065_rawchem_etc \
+    11_dsstoxsubstance 12_habits_and_practices \
+    13_habits_and_practices_to_puc 14_product_to_puc 18_puc_tag
+
 
 # some shells prefer one line
-python manage.py loaddata 00_superuser 01_lookups 02_datasource 03_datagroup 04_PUC 05_product 06_datadocument 065_rawchem_etc 07_script 08_extractedtext 09_productdocument  11_dsstoxsubstance 12_habits_and_practices 13_habits_and_practices_to_puc 14_product_to_puc 15_extractedfunctionaluse  18_puc_tag
+python manage.py loaddata 00_superuser 01_lookups 02_datasource 03_datagroup 04_PUC 05_product 06_datadocument  07_script 08_extractedtext 16_extractedcpcat 09_productdocument 065_rawchem_etc 11_dsstoxsubstance 12_habits_and_practices 13_habits_and_practices_to_puc 14_product_to_puc   18_puc_tag

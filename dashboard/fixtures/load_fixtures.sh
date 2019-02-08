76b6c04d363fda99231364dfe2115c95226f0239
Matt Frost <mwfrost@fastmail.fm>
2019-02-08 12:06:05 -0800

    Correct fixture-loading order

--- dashboard/fixtures/load_fixtures.sh
+++ dashboard/fixtures/load_fixtures.sh
@@ -13,4 +13,4 @@ python manage.py loaddata 00_superuser 01_lookups \
     17_extractedlistpresence 18_puc_tag
 
 # some shells prefer one line
-python manage.py loaddata 00_superuser 01_lookups 02_datasource 03_datagroup 04_PUC 05_product 06_datadocument 065_rawchem_etc 07_script 08_extractedtext 09_productdocument  12_habits_and_practices 13_habits_and_practices_to_puc 14_product_to_puc 15_extractedfunctionaluse  18_puc_tag
+python manage.py loaddata 00_superuser 01_lookups 02_datasource 03_datagroup 04_PUC 05_product 06_datadocument 07_script 08_extractedtext 16_extractedcpcat 065_rawchem_etc 09_productdocument  12_habits_and_practices 13_habits_and_practices_to_puc 14_product_to_puc 15_extractedfunctionaluse  18_puc_tag

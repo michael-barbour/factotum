056bfcef1ed00063e67402766cd7d73e9d994ba0
Matt Frost <mwfrost@fastmail.fm>
2019-02-11 06:31:29 -0800

    Start figuring out how to use ephemeral test indexes

--- dashboard/tests/functional/test_search.py
+++ dashboard/tests/functional/test_search.py
@@ -2,11 +2,20 @@ from django.test import TestCase
 from dashboard.tests.loader import load_model_objects, fixtures_standard
 from dashboard.models import DataDocument
 from dashboard.documents import DataDocumentDocument
+from django_elasticsearch_dsl import DocType, Index
+
 
 class TestSearch(TestCase):
-    fixtures = fixtures_standard
 
-    def test_datadoc_index():
+    def setUp(self):
+        """
+        destroy the main index and repopulate it with the test fixture
+        """
+        datadocs = Index('datadocs')
+        datadocs_bkp = datadocs.clone('datadocs-bkp')
+        print(datadocs_bkp.__dict__)
+
+    def test_datadoc_index(self):
         docsearch = DataDocumentDocument.search().query("match", title="Depot")
         for hit in docsearch:
             print(

dfa90cc9e8056e61efae373b5bb6eff1836a5f02
Matt Frost <mwfrost@fastmail.fm>
2019-02-11 12:49:28 -0800

    Attempting to swap in some test-only indexes at runtime

--- dashboard/tests/functional/test_search.py
+++ dashboard/tests/functional/test_search.py
@@ -4,19 +4,38 @@ from dashboard.models import DataDocument
 from dashboard.documents import DataDocumentDocument
 from django_elasticsearch_dsl import DocType, Index
 
-
 class TestSearch(TestCase):
+
+    INDEX_SUFFIX = '-test'
+    testindexname = f'datadocs%s' % INDEX_SUFFIX
 
     def setUp(self):
         """
-        destroy the main index and repopulate it with the test fixture
+        clone indexes and populate them with fixture data
         """
         datadocs = Index('datadocs')
-        datadocs_bkp = datadocs.clone('datadocs-bkp')
-        print(datadocs_bkp.__dict__)
+        if Index(self.testindexname).exists():
+            Index(self.testindexname).delete()
+        datadocs_test = datadocs.clone(self.testindexname)
+        datadocs_test.create()
+
+        datadocs_test.settings(read_only_allow_delete="false")
+        # at this point, we need to tell elasticsearch-dsl that the objects being added 
+        # to the local_factotum_test database should be indexed into the datadocs-test index
+        load_model_objects()
+        
+
+    def tearDown(self):
+        """
+        remove the test indexes
+        """
+        datadocs_test = Index(self.testindexname)
+        datadocs_test.delete()
 
     def test_datadoc_index(self):
-        docsearch = DataDocumentDocument.search().query("match", title="Depot")
+        docsearch = DataDocumentDocument.search(index=self.testindexname).query("match", title="Depot")
+
+        print(DataDocumentDocument.search(index=self.testindexname).__dict__)
         for hit in docsearch:
             print(
                 "filename : {}, title {}".format(hit.filename, hit.title)

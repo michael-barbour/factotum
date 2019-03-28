1dce9ff52e11330324277bf8a2ea0017ec855e5e
mwfrost <mwfrost@gmail.com>
2019-03-28 16:03:10 -0400

    657 csv datadocuments (#768)
    * #657 Change file icon on datadocument detail page
    
    * #657 Reconcile document types with PROD
    
    * #657 add a test for a csv file as a doc in the uploaded list
    
    Also autopep8 changes
    
    * #657 Add additional check to test
    
    * #657 Add html case to test suite

--- dashboard/tests/functional/test_datadocument_create.py
+++ dashboard/tests/functional/test_datadocument_create.py
@@ -13,7 +13,6 @@ from django.core.files.uploadedfile import (InMemoryUploadedFile,
 from dashboard.tests.loader import fixtures_standard
 
 
-
 class DDTestModel(TestCase):
 
     fixtures = fixtures_standard
@@ -23,17 +22,23 @@ class DDTestModel(TestCase):
 
     def test_dd_model_with_wrong_document_type(self):
         # Choose a Composition group
-        dgcomp = DataGroup.objects.filter(group_type__code='CO').first()
+        dgcomp = DataGroup.objects.filter(
+            group_type__title='Composition').first()
         # Choose a document type from the wrong parent group type
-        dt_fu = DocumentType.objects.filter(group_type__code='FU').first()
-        dd = DataDocument.objects.create(filename="some.pdf", title="My Document", document_type = dt_fu , data_group=dgcomp)
+        dt_fu = DocumentType.objects.filter(
+            group_type__title='Functional use').first()
+        dd = DataDocument.objects.create(
+            filename="some.pdf", title="My Document", document_type=dt_fu, data_group=dgcomp)
         with self.assertRaises(ValidationError):
             dd.save()
             dd.full_clean()
-        dt_comp = DocumentType.objects.filter(group_type__code='CO').first()
-        dd = DataDocument.objects.create(filename="some.pdf", title="My Document", document_type = dt_comp , data_group=dgcomp)
+        dt_comp = DocumentType.objects.filter(
+            group_type__title='Composition').first()
+        dd = DataDocument.objects.create(
+            filename="some.pdf", title="My Document", document_type=dt_comp, data_group=dgcomp)
         dd.save()
-        self.assertEqual(dt_comp.title, dd.document_type.title )
+        self.assertEqual(dt_comp.title, dd.document_type.title)
+
 
 class DDTestUpload(TestCase):
 
@@ -52,32 +57,33 @@ class DDTestUpload(TestCase):
         csv_len = len(csv_string_good)
 
         sample_csv = InMemoryUploadedFile(data,
-                                            field_name='csv',
-                                            name='register_records.csv',
-                                            content_type='text/csv',
-                                            size=csv_len,
-                                            charset='utf-8')
-        form_data= {'name': ['Composition Type Group'],
-                    'description': ['test data group'],
-                    'group_type': ['2'], # Composition
-                    'downloaded_by': ['1'],
-                    'downloaded_at': ['08/02/2018'],
-                    'download_script': ['1'],
-                    'data_source': ['10']}
+                                          field_name='csv',
+                                          name='register_records.csv',
+                                          content_type='text/csv',
+                                          size=csv_len,
+                                          charset='utf-8')
+        form_data = {'name': ['Composition Type Group'],
+                     'description': ['test data group'],
+                     'group_type': ['2'],  # Composition
+                     'downloaded_by': ['1'],
+                     'downloaded_at': ['08/02/2018'],
+                     'download_script': ['1'],
+                     'data_source': ['10']}
         request = self.factory.post(path='/datagroup/new', data=form_data)
         request.FILES['csv'] = sample_csv
         request.user = User.objects.get(username='Karyn')
-        request.session={}
+        request.session = {}
         request.session['datasource_title'] = 'Walmart'
         request.session['datasource_pk'] = 10
-        resp = views.data_group_create(pk = 10, request=request)
-        self.assertEqual(resp.status_code,302,
-                        "Should be redirected to new datagroup detail page")
+        resp = views.data_group_create(pk=10, request=request)
+        self.assertEqual(resp.status_code, 302,
+                         "Should be redirected to new datagroup detail page")
         # does the datagroup in the ORM contain the new data docs?
         newdg_pk = resolve(resp.url).kwargs['pk']
         newdg = DataGroup.objects.get(pk=newdg_pk)
         newdds = DataDocument.objects.filter(data_group=newdg)
-        self.assertEqual(newdds.count(),2,'There should be two new data documents')
+        self.assertEqual(newdds.count(), 2,
+                         'There should be two new data documents')
 
     def testBadGroupTypeInCSV(self):
         csv_string_bad = ("filename,title,document_type,url,organization\n"
@@ -89,24 +95,111 @@ class DDTestUpload(TestCase):
         csv_len = len(csv_string_bad)
 
         sample_csv = InMemoryUploadedFile(data,
-                                            field_name='csv',
-                                            name='register_records.csv',
-                                            content_type='text/csv',
-                                            size=csv_len,
-                                            charset='utf-8')
-        form_data= {'name': ['Composition Type Group'],
-                    'description': ['test data group'],
-                    'group_type': ['2'], # Composition
-                    'downloaded_by': ['1'],
-                    'downloaded_at': ['08/02/2018'],
-                    'download_script': ['1'],
-                    'data_source': ['10']}
+                                          field_name='csv',
+                                          name='register_records.csv',
+                                          content_type='text/csv',
+                                          size=csv_len,
+                                          charset='utf-8')
+        form_data = {'name': ['Composition Type Group'],
+                     'description': ['test data group'],
+                     'group_type': ['2'],  # Composition
+                     'downloaded_by': ['1'],
+                     'downloaded_at': ['08/02/2018'],
+                     'download_script': ['1'],
+                     'data_source': ['10']}
         request = self.factory.post(path='/datagroup/new', data=form_data)
         request.FILES['csv'] = sample_csv
         request.user = User.objects.get(username='Karyn')
-        request.session={}
+        request.session = {}
         request.session['datasource_title'] = 'Walmart'
         request.session['datasource_pk'] = 10
-        resp = views.data_group_create(pk = 10 , request=request)
+        resp = views.data_group_create(pk=10, request=request)
         # the upload form should be invalid
         self.assertIn('CSV has bad data in row/s:'.encode(), resp.content)
+
+    def test_upload_csv_as_datadoc(self):
+        csv_string = ("filename,title,document_type,url,organization\n"
+                           "Cal_Pesticide_Residues_1987.csv,Example Datadocument from CSV,SG,, \n"
+                           )
+
+        data = io.StringIO(csv_string)
+        csv_len = len(csv_string)
+
+        sample_csv = InMemoryUploadedFile(data,
+                                          field_name='csv',
+                                          name='register_records.csv',
+                                          content_type='text/csv',
+                                          size=csv_len,
+                                          charset='utf-8')
+        form_data = {'name': ['California Pesticides'],
+                     'description': ['test data group'],
+                     'group_type': ['6'],  # CPCat
+                     'downloaded_by': ['1'],
+                     'downloaded_at': ['08/02/2018'],
+                     'download_script': ['1'],
+                     'data_source': ['10']}
+        request = self.factory.post(path='/datagroup/new', data=form_data)
+        request.FILES['csv'] = sample_csv
+        request.user = User.objects.get(username='Karyn')
+        request.session = {}
+        request.session['datasource_title'] = 'Walmart'
+        request.session['datasource_pk'] = 10
+        resp = views.data_group_create(pk=10, request=request)
+        self.assertEqual(resp.status_code, 302,
+                         "Should be redirected to new datagroup detail page")
+        newdg_pk = resolve(resp.url).kwargs['pk']
+
+        # does the datagroup in the ORM contain the new data docs?
+        newdg = DataGroup.objects.get(pk=newdg_pk)
+        newdds = DataDocument.objects.filter(data_group=newdg)
+        self.assertEqual(newdds.count(), 1,
+                         'There should be one new data document')
+
+        # Does the data document page include a link to the csv?  
+        resp = self.client.get(f'/datadocument/%s/' % newdds[0].id)            
+        # Does the response include a link to the data document's file?
+        self.assertContains(resp, f'pdf/document_%s.csv' % newdds[0].id)
+        
+        
+    def test_upload_html_as_datadoc(self):
+        csv_string = ("filename,title,document_type,url,organization\n"
+                           "alberto_balsam_conditioner_antioxidant_blueberry.html,Example Datadocument from HTML,ID,, \n"
+                           )
+
+        data = io.StringIO(csv_string)
+        csv_len = len(csv_string)
+
+        sample_csv = InMemoryUploadedFile(data,
+                                          field_name='csv',
+                                          name='register_records.csv',
+                                          content_type='text/csv',
+                                          size=csv_len,
+                                          charset='utf-8')
+        form_data = {'name': ['California Pesticides'],
+                     'description': ['test data group'],
+                     'group_type': ['2'],  # Composition
+                     'downloaded_by': ['1'],
+                     'downloaded_at': ['08/02/2018'],
+                     'download_script': ['1'],
+                     'data_source': ['10']}
+        request = self.factory.post(path='/datagroup/new', data=form_data)
+        request.FILES['csv'] = sample_csv
+        request.user = User.objects.get(username='Karyn')
+        request.session = {}
+        request.session['datasource_title'] = 'Walmart'
+        request.session['datasource_pk'] = 10
+        resp = views.data_group_create(pk=10, request=request)
+        self.assertEqual(resp.status_code, 302,
+                         "Should be redirected to new datagroup detail page")
+        newdg_pk = resolve(resp.url).kwargs['pk']
+
+        # does the datagroup in the ORM contain the new data docs?
+        newdg = DataGroup.objects.get(pk=newdg_pk)
+        newdds = DataDocument.objects.filter(data_group=newdg)
+        self.assertEqual(newdds.count(), 1,
+                         'There should be one new data document')
+
+        # Does the data document page include a link to the csv?  
+        resp = self.client.get(f'/datadocument/%s/' % newdds[0].id)            
+        # Does the response include a link to the data document's file?
+        self.assertContains(resp, f'pdf/document_%s.html' % newdds[0].id)

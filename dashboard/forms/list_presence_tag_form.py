e251a4fb1995963345045ff8c96b7dae4b60ef35
Zach <zach.munzenrider@csra.com>
2019-05-07 18:26:21 -0400

    prevent invalid lp keywords from being submitted

--- dashboard/forms/list_presence_tag_form.py
+++ dashboard/forms/list_presence_tag_form.py
@@ -1,5 +1,4 @@
-from django import forms
-from dashboard.models import ExtractedListPresence
+from dashboard.models import ExtractedListPresence, ExtractedListPresenceTag
 from dal import autocomplete
 
 
@@ -21,3 +20,14 @@ class ExtractedListPresenceTagForm(autocomplete.FutureModelForm):
                                                  'data-minimum-input-length': 3})
         self.fields['tags'].label = 'List Presence Keywords'
         self.fields['tags'].help_text = ''
+
+    def clean(self):
+        valid_tag_list = ExtractedListPresenceTag.objects.all().values_list('name', flat=True)
+        self.invalid_tags = []
+        for tag in self.cleaned_data['tags']:
+            if tag in valid_tag_list:
+                pass
+            else:
+                self.invalid_tags.append(tag)
+                self.cleaned_data['tags'].remove(tag)
+                self.add_error('tags', '%s is not a valid keyword' % tag)

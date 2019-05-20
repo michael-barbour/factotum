00a1d0bca1f88db9661d42f8e014e8130469d9b6
Matt Frost <mwfrost@fastmail.fm>
2019-05-20 14:02:21 -0700

    Remove openiconic files from static folder

--- dashboard/tests/functional/test_datagroup_detail.py
+++ dashboard/tests/functional/test_datagroup_detail.py
@@ -141,13 +141,13 @@ class DataGroupDetailTest(TestCase):
     def test_delete_doc_button(self):
         url = f'/datagroup/{DataGroup.objects.first().id}/'
         response = self.client.get(url).content.decode('utf8')
-        span = '<span class="fa fa-trash-o"></span>'
+        span = '<span class="fa fa-trash">'
         self.assertIn(span, response,
                       'Trash button should be present if not matched.')
         self.objects.doc.matched = True
         self.objects.doc.save()
         response = self.client.get(url).content.decode('utf8')
-        span = '<span class="fa fa-fs fa-check-circle" style="color:green;"></span>'
+        span = '<span class="fa fa-check-circle" style="color:green;"></span>'
         self.assertIn(span, response,
                       'Check should be present if matched.')
 

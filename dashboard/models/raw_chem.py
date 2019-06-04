e928286d7016e3acb3e869df9f17d8188a9e27bc
Matt Frost <mwfrost@fastmail.fm>
2019-06-04 12:06:21 -0700

    Added long example chemical
    From http://www.worldwidewords.org/qa/qa-imm1.htm

--- dashboard/models/raw_chem.py
+++ dashboard/models/raw_chem.py
@@ -10,7 +10,7 @@ class RawChem(models.Model):
         on_delete=models.CASCADE, null=False, blank = False)
 
     raw_cas = models.CharField("Raw CAS", max_length=100, null=True, blank=True)
-    raw_chem_name = models.CharField("Raw chemical name", max_length=500,
+    raw_chem_name = models.CharField("Raw chemical name", max_length=1300,
                                                         null=True, blank=True)
     temp_id = models.IntegerField(default=0, null=True, blank=True)
     temp_obj_name = models.CharField(max_length=255, null=True, blank=True)

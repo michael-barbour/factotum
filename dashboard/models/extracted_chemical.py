7eff804844da714553809ae4fe85a32b8f6297f8
tad larsen <justatad44@users.noreply.github.com>
2019-06-18 09:28:18 -0700

    848 Fixed issue with ingredient_rank sorting (#933)
    * 848 Fixed issue with ingredient_rank sorting
    
    * 848_ingredient_rank_sorting - fixed test and replaced rawchem yaml file
    
    * 848_ingredient_rank_sorting - included migration file

--- dashboard/models/extracted_chemical.py
+++ dashboard/models/extracted_chemical.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from .unit_type import UnitType
 from .weight_fraction_type import WeightFractionType
 from .raw_chem import RawChem
-
+from django.db.models import F
 
 def validate_ingredient_rank(value):
     if value < 1 or value > 999:
@@ -26,6 +26,10 @@ class ExtractedChemical(CommonInfo, RawChem):
     ingredient_rank = models.PositiveIntegerField("Ingredient rank", null=True, blank=True,
                                                   validators=[validate_ingredient_rank])
     raw_central_comp = models.CharField("Raw central composition", max_length=100, null=True, blank=True)
+
+    class Meta:
+        ordering = (F('ingredient_rank').asc(nulls_last=True), )
+
 
     def __str__(self):
         return str(self.raw_chem_name) if self.raw_chem_name else ''

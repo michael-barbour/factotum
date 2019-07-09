18f8b00f5176cec265e3747fa0cb782aac367a7f
Zach <zach.munzenrider@csra.com>
2019-07-09 12:29:49 -0400

    post-lint-merge cleanup

--- dashboard/forms/__init__.py
+++ dashboard/forms/__init__.py
@@ -1,4 +1,28 @@
-from .forms import *
-from dashboard.forms.list_presence_tag_form import *
-from dashboard.forms.product_tag_form import *
-from dashboard.forms.chemical_curation import *
+from .forms import (
+    DataGroupForm,
+    ExtractionScriptForm,
+    CleanCompDataForm,
+    DataSourceForm,
+    PriorityForm,
+    QANotesForm,
+    ExtractedTextQAForm,
+    ProductLinkForm,
+    ProductForm,
+    ProductViewForm,
+    BulkProductPUCForm,
+    BulkProductTagForm,
+    ExtractedTextForm,
+    ExtractedCPCatForm,
+    ExtractedCPCatEditForm,
+    ExtractedHHDocForm,
+    ExtractedHHDocEditForm,
+    DocumentTypeForm,
+    ExtractedChemicalFormSet,
+    ExtractedChemicalForm,
+    create_detail_formset,
+    DataDocumentForm,
+)
+from dashboard.forms.list_presence_tag_form import ExtractedListPresenceTagForm
+from dashboard.forms.product_tag_form import ProductTagForm
+from dashboard.forms.chemical_curation import DataGroupSelector
+from dashboard.forms.puc_forms import ProductPUCForm, HabitsPUCForm, BulkPUCForm

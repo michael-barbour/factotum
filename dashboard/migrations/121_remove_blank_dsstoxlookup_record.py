86a2d6618b69e1cd53b4aa43016db69b9aed3939
Zach Munzenrider <33101677+zachmunzenrider@users.noreply.github.com>
2019-08-26 16:50:12 -0400

    1025 remove blank dsstoxlookup record (#1044)
    * fixing prod_type display on PUC page
    
    * migration to remove blank dsstoxlookup record, id 1090

---
+++ dashboard/migrations/121_remove_blank_dsstoxlookup_record.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("dashboard", "0120_auto_20190712_1054")]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            UPDATE dashboard_rawchem
+            SET dsstox_id = NULL
+            WHERE dsstox_id = 1090
+            """,
+        ),
+        migrations.RunSQL(
+            """
+            DELETE FROM dashboard_dsstoxlookup
+            WHERE id = 1090
+            """,
+        ),
+    ]

56ebedd78ba47de2158d80cb600164193001f0d5
Matt Covalt <mcovalt@mailbox.org>
2019-11-21 19:35:16 -0500

    Another media fix

--- factotum/settings.py
+++ factotum/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 LOGIN_REDIRECT_URL = "index"
 LOGIN_URL = "login"

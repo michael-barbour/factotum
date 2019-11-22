b42cadfad1ebdc6543339888d021a6e1b6eb59b0
Matt Frost <mwfrost@fastmail.fm>
2019-11-22 07:18:46 -0800

    Add a custom filter for looking up a dict key in a template

--- dashboard/templatetags/dashboard_extras.py
+++ dashboard/templatetags/dashboard_extras.py
@@ -2,6 +2,8 @@ import os
 from django import template
 
 register = template.Library()
+from django.template.defaultfilters import register
+
 
 ICON_MAP = {
     ".csv": "-csv",
@@ -20,3 +22,9 @@ ICON_MAP = {
 def fileicon(value):
     _, ext = os.path.splitext(value)
     return "fa-file" + ICON_MAP.get(ext, "")
+
+
+@register.filter(name='dict_key')
+def dict_key(d, k):
+    '''Returns the given key from a dictionary.'''
+    return d.get(k)
